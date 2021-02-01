f55ad5ed2fdc440c8c8b6aef620efaa0155ba16b
Joao Farias <joao@thatsabug.com>
2021-02-02 00:26:26 +0100

    Makefile & CircleCi

--- src/test/kotlin/petstore/actors/PetStore.kt
+++ src/test/kotlin/petstore/actors/PetStore.kt
@@ -1,11 +1,17 @@
 package petstore.actors
 
+import assertk.assertThat
+import assertk.assertions.isFailure
+import assertk.assertions.messageContains
+import com.google.gson.Gson
+import io.restassured.http.ContentType
 import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.apache.http.HttpStatus
 import petstore.models.Pet
+import java.lang.AssertionError
 
 class PetStore {
     fun fetchPetsByStatus(status: String): Set<Pet> =
@@ -18,4 +24,54 @@ class PetStore {
         }. Extract {
             body().`as`(Array<Pet>::class.java)
         }.toSet()
+
+    fun addPetToStore(pet: Pet): Pet =
+        Given {
+            contentType(ContentType.JSON)
+                .body(makePetJson(pet))
+        }. When {
+            post("https://petstore.swagger.io/v2/pet")
+        }. Then {
+            statusCode(HttpStatus.SC_OK)
+        }. Extract {
+            body().`as`(Pet::class.java)
+        }
+
+    private fun makePetJson(pet: Pet) =
+        Gson().toJson(pet, Pet::class.java)
+
+    fun updatePetInStore(pet: Pet) =
+        Given {
+            contentType(ContentType.JSON)
+                .body(makePetJson(pet))
+        }. When {
+            put("https://petstore.swagger.io/v2/pet")
+        }. Then {
+            statusCode(HttpStatus.SC_OK)
+        }. Extract {
+            body().`as`(Pet::class.java)
+        }
+
+    fun deletePet(pet: Pet) {
+        When {
+            delete("https://petstore.swagger.io/v2/pet/${pet.id}")
+        }. Then {
+            statusCode(HttpStatus.SC_OK)
+        }
+    }
+
+    fun isPetMissingInStore(pet: Pet): Boolean {
+        return try {
+            When {
+                get("https://petstore.swagger.io/v2/pet/${pet.id}")
+            }. Then {
+                statusCode(HttpStatus.SC_NOT_FOUND)
+            }
+            true
+        } catch (e: AssertionError){
+            false
+        }
+    }
+
 }
+
--- src/test/kotlin/petstore/steps/ChangePetsSteps.kt
+++ src/test/kotlin/petstore/steps/ChangePetsSteps.kt
@@ -3,29 +3,35 @@ package petstore.steps
 import assertk.assertAll
 import assertk.assertThat
 import assertk.assertions.isFailure
+import assertk.assertions.isTrue
 import assertk.assertions.messageContains
 import com.google.gson.Gson
 import io.cucumber.java8.En
+import io.cucumber.java8.HookNoArgsBody
 import io.restassured.http.ContentType
 import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.apache.http.HttpStatus
+import petstore.actors.PetStore
 import petstore.models.*
 
 class ChangePetsSteps: En {
 
 
+    private lateinit var petStore: PetStore
     private lateinit var returnedPet: Pet
 
     init {
+        Before(HookNoArgsBody { startStore() })
+
         Given("the user adds a new pet with {string} name, {string}, and {string} status") {
                 name: String, category: String, status: String ->
             run {
                 val petToSave = createPet(name, category, status)
 
-                returnedPet = addPetToStore(
+                returnedPet = petStore.addPetToStore(
                     petToSave
                 )
 
@@ -41,7 +47,7 @@ class ChangePetsSteps: En {
             newStatus: String ->
             run {
                 val updatedPet = returnedPet.copy(status = newStatus)
-                returnedPet = updatePetInStore(updatedPet)
+                returnedPet = petStore.updatePetInStore(updatedPet)
 
                 assertAll {
                     assertThat(returnedPet).`has same id as`(updatedPet)
@@ -52,60 +58,19 @@ class ChangePetsSteps: En {
         }
 
         When("the user deletes the pet") {
-            deletePet(returnedPet)
+            petStore.deletePet(returnedPet)
         }
 
         Then("the pet no longer exists") {
-            assertThat {
-                fetchPetById(returnedPet)
-            }.isFailure().messageContains("<200> but was <404>")
+            assertThat(petStore.isPetMissingInStore(returnedPet)).isTrue()
         }
+    }
+
+    private fun startStore() {
+        petStore = PetStore()
     }
 
     private fun createPet(name: String, category: String, status: String) =
         Pet(name = name, category = PetCategory(name = category), status = status)
 
-    private fun addPetToStore(pet: Pet) =
-        Given {
-            contentType(ContentType.JSON)
-                .body(makePetJson(pet))
-        }. When {
-            post("https://petstore.swagger.io/v2/pet")
-        }. Then {
-            statusCode(HttpStatus.SC_OK)
-        }. Extract {
-            body().`as`(Pet::class.java)
-        }
-
-    private fun makePetJson(pet: Pet) =
-        Gson().toJson(pet, Pet::class.java)
-
-    private fun updatePetInStore(pet: Pet) =
-        Given {
-            contentType(ContentType.JSON)
-                .body(makePetJson(pet))
-        }. When {
-            put("https://petstore.swagger.io/v2/pet")
-        }. Then {
-            statusCode(HttpStatus.SC_OK)
-        }. Extract {
-            body().`as`(Pet::class.java)
-        }
-
-    private fun deletePet(pet: Pet) {
-        When {
-            delete("https://petstore.swagger.io/v2/pet/${pet.id}")
-        }. Then {
-            statusCode(HttpStatus.SC_OK)
-        }
-    }
-
-    private fun fetchPetById(pet: Pet) =
-        When {
-            get("https://petstore.swagger.io/v2/pet/${pet.id}")
-        }. Then {
-            statusCode(HttpStatus.SC_OK)
-        }
 }
-
-
